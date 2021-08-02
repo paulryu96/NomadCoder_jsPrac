08e05add73835f8a18002c93049f317c7482b784
PaulRyu <40940136+paulryu96@users.noreply.github.com>
2021-08-02 16:24:32 +0900

    Update conditional.js

--- conditional.js
+++ conditional.js
@@ -1,9 +1,11 @@
 function checkUserAge(){
   const typeAge = parseInt(document.getElementById('typeAge').value)
 
-  if (typeAge > 18){
+  if (isNaN(typeAge)){
+    document.querySelector('li').innerText = "나이를 입력하세요."
+  } else if (typeAge > 18){
     document.querySelector('li').innerText = "성인입니다."
-  } else {
+  } else if (typeAge <= 18){
     document.querySelector('li').innerText = '미성년자입니다.'
   }
 
