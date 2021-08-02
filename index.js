853bac56e3f8eb379a8e5fd2ad8143340e517d4d
PaulRyu <40940136+paulryu96@users.noreply.github.com>
2021-08-02 14:25:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const calculator = {
+  add: function(a, b){
+    console.log(a + b)
+  }
+  ,
+  mul: function(a,b){
+    console.log(a * b)
+  },
+
+  powerOf: function(a, b){
+    console.log(a ** b)
+  },
+
+  sub: function(a, b){
+    console.log(a - b)
+  },
+
+  div: function(a,b){
+    console.log(a / b)
+  }}
+
+calculator.sub(5, 3)
+calculator.add(1, 3)
